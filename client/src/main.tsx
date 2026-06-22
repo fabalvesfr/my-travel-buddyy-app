53fc92c6c06fa20827eace7f90ab45bcc929da49
Fabio F. <fabio9162@gmail.com>
2026-06-22 12:27:52 -0300

    feat: frontend bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
-createRoot(document.getElementById('root')!).render(
+const GRAPHQL_ENDPOINT = "http://localhost:4000/";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: GRAPHQL_ENDPOINT });
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
-)
+);
